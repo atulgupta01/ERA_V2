ca8a7fbc6e1455aea07b2c92aa88c96a57996496
Atul Gupta <samatul@gmail.com>
2024-06-23 07:47:53 +0530

    Assignment 20
    Assignment 20

---
+++ Assignment_20/app.py
@@ -0,0 +1,41 @@
+import pickle
+import gradio as gr
+
+from train_util import get_stats, merge, encode, decode
+
+with open('model.pkl', 'rb') as handle:
+    model = pickle.load(handle)
+
+compiled_pattern = model['compiled_pattern']
+merges = model['merges']
+vocab = model['vocab']
+
+def generate_tokens(hindi_txt):
+  enc_text = encode(hindi_txt, merges, compiled_pattern)
+  txt = decode(enc_text, vocab)
+  v_mapping = [(str(i),decode([i], vocab)) for i in enc_text]
+  enc_text = ', '.join([str(i) for i in enc_text])
+  return (enc_text, txt, v_mapping)
+  
+with gr.Blocks() as demo:
+    gr.HTML("<h1 align = 'center'> Token Generation for Hindi Dataset </h1>")
+
+    with gr.Row():
+      with gr.Column():
+        inputs = [gr.TextArea(label = "Enter initial text to generate tokens in Hindi", lines = 10)]
+        generate_btn = gr.Button(value = 'Generate Text')
+      with gr.Column():
+        enc = gr.Textbox(label = "Encoded Token")
+        txt = gr.Textbox(label = "Decoded Text")
+        map = gr.Textbox(label = "Mapping of the text")
+        outputs  = [
+            enc,
+            txt,
+            map
+            ]
+    
+    generate_btn.click(fn = generate_tokens, inputs= inputs, outputs = outputs)
+
+
+if __name__ == '__main__':
+    demo.launch() 
